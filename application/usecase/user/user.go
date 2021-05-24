d276bf3195a9ceaeb2b426bfbaaff2494618e621
Ryutaro95 <ryutaro9595@gmail.com>
2021-05-25 05:32:41 +0900

    Add: User usecase

---
+++ application/usecase/user/user.go
@@ -0,0 +1,28 @@
+package usecase
+
+import (
+	"github.com/Ryutaro95/domain/model"
+	repository "github.com/Ryutaro95/domain/repository/user"
+)
+
+type UserUsecase interface {
+	Create(firstname, lastname, nickname string, age int) (*model.User, error)
+}
+
+type userUsecase struct {
+	userRepo repository.UserRepository
+}
+
+func NewUserUsecase(userRepo repository.UserRepository) UserUsecase {
+	return &userUsecase{userRepo: userRepo}
+}
+
+func (uu *userUsecase) Create(firstname, lastname, nickname string, age int) (*model.User, error) {
+	user, err := model.NewUser(firstname, lastname, nickname, age)
+	if err != nil {
+		return nil, err
+	}
+
+	createdUser := uu.userRepo.Create(user)
+	return createdUser, nil
+}
